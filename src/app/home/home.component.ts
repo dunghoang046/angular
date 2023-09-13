bab7ad74ea8133ff5a023e744b274e28e7195360
dunghv <dunghv@tinducsoftware.com>
2023-09-13 16:43:58 +0700

    cm

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,17 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent  implements OnInit{
- public name="Dũng Hoàng";
- public age=19;
- public traicay=['Táo','Nho','Cam'];
-  constructor(){}
+export class HomeComponent implements OnInit {
+  public name = "Dũng Hoàng";
+  public age = 19;
+  public traicay = ['Táo', 'Nho', 'Cam'];
+  public traicay2 = [{ ten: 'Táo', gia: 12 }, { ten: 'Nho', gia: 300 }, { ten: 'Cam', gia: 350 }];
+  constructor() { }
   ngOnInit(): void {
-    
+
   }
- public resetName(): void{
+  public resetName(): void {
     console.log("resetName");
-    this.name='';
-    console.log("Trái cây:"+this.traicay)
+    this.name = '';
+    console.log("Trái cây:" + this.traicay)
   }
 }
