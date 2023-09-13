5907b16498ba1c1c5c622e91ef2d001cec996ee3
dunghv <dunghv@tinducsoftware.com>
2023-09-13 15:20:40 +0700

    cm

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent  implements OnInit{
  public name="Dũng Hoàng";
+ public age=19;
   constructor(){}
   ngOnInit(): void {
     
